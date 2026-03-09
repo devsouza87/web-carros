9842be099b59a6770800047b6edefc92edd74ff7
Cesar Augusto <cesaraugusto.87@hotmail.com>
2026-03-09 11:46:13 -0300

    adiciona o componente header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,34 @@
+import { FiLogIn, FiUser } from "react-icons/fi";
+import logo from "../../assets/logo.svg";
+import { Link } from "react-router";
+
 export const Header = () => {
-  return <div>Header</div>;
+  const signed = true;
+  const loadingAuth = false;
+
+  return (
+    <header className="w-full h-16 flex items-center justify-center bg-white drop-shadow mb-4">
+      <div className="w-full max-w-7xl h-full flex items-center justify-between px-4 mx-auto">
+        <Link to="/">
+          <img src={logo} alt="Logo Web Carros" />
+        </Link>
+
+        {!loadingAuth && signed && (
+          <Link to="/cars">
+            <div className="border rounded-full p-2">
+              <FiUser size={24} color="#000" />
+            </div>
+          </Link>
+        )}
+
+        {!loadingAuth && !signed && (
+          <Link to="/cars">
+            <div className="border rounded-full p-2">
+              <FiLogIn size={24} color="#000" />
+            </div>
+          </Link>
+        )}
+      </div>
+    </header>
+  );
 };
